bbb64594465b9e943a5f9dfe4425a5155d2a7ea2
Ashish Prajapati <ashishanshprajapati@gmail.com>
2026-02-09 16:33:47 +0530

    feat(script): add interactive Y/N prompt

--- start.sh
+++ start.sh
@@ -8,3 +8,19 @@ WALLET_PATH=""
 WALLET_PUBKEY=""
 TOKEN_MINT=""
 TOKEN_ACCOUNT=""
+
+ask_yes_no() {
+  local prompt="$1"
+  local default="${2:-N}"
+  local input
+
+  if [[ "$default" == "Y" ]]; then
+    read -r -p "$prompt [Y/n]: " input
+    input="${input:-Y}"
+  else
+    read -r -p "$prompt [y/N]: " input
+    input="${input:-N}"
+  fi
+
+  [[ "$input" =~ ^[Yy]$ ]]
+}
